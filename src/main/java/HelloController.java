562458914cb10540f6cda2b1eceef0750d32f548
Goliathuzzzz <mlaakkonen@live.com>
2025-03-27 12:02:49 +0200

    Added error handling

--- src/main/java/HelloController.java
+++ src/main/java/HelloController.java
@@ -10,7 +10,7 @@ import java.util.ResourceBundle;
 public class HelloController {
 
     @FXML
-    private Label lblDistance, lblFuel, lblFuelConsumption, lblResult;
+    private Label lblDistance, lblFuel, lblFuelConsumption, lblResult, lblErrorDistance, lblErrorFuel;
 
     @FXML
     private TextField distanceInput, fuelInput;
@@ -27,6 +27,7 @@ public class HelloController {
 
     public void initialize() {
         fuelConsumptionCalc = new FuelConsumptionCalc();
+        rb = ResourceBundle.getBundle("messages", new Locale("en", "US"));
     }
 
     private void setLanguage(Locale locale) {
@@ -35,9 +36,24 @@ public class HelloController {
 
     @FXML
     private void handleCalculateFuel() {
-        double distance = Double.parseDouble(distanceInput.getText());
-        double fuel = Double.parseDouble(fuelInput.getText());
-        double consumption = fuelConsumptionCalc.calculateFuelConsumption(distance, fuel);
+        double distance = 0;
+        double fuel = 0;
+        double consumption = 0;
+        try {
+            distance = Double.parseDouble(distanceInput.getText());
+        } catch (NumberFormatException e) {
+            lblErrorDistance.setText(rb.getString("error"));
+        }
+        try {
+            fuel = Double.parseDouble(fuelInput.getText());
+        } catch (NumberFormatException e) {
+            lblErrorFuel.setText(rb.getString("error"));
+        }
+        try {
+            consumption = fuelConsumptionCalc.calculateFuelConsumption(distance, fuel);
+        } catch (Exception e) {
+            lblResult.setText(rb.getString("error"));
+        }
         String formattedConsumption = String.format("%.2f", consumption);
         lblResult.setText(" " + formattedConsumption + " l/100Km ");
     }
@@ -73,5 +89,11 @@ public class HelloController {
         lblFuel.setText(resourceBundle.getString("fuel_used"));
         btnCalculate.setText(resourceBundle.getString("calculate_fuel_consumption"));
         lblFuelConsumption.setText(resourceBundle.getString("fuel_consumption"));
+        if (!lblErrorFuel.getText().isEmpty()) {
+            lblErrorFuel.setText(resourceBundle.getString("error"));
+        }
+        if (!lblErrorDistance.getText().isEmpty()) {
+            lblErrorDistance.setText(resourceBundle.getString("error"));
+        }
     }
 }
